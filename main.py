99faed3ffcafb863a94c61287238aadaf5122e12
Siddharth Desai <siddharth.desai1996@gmail.com>
2021-02-05 13:46:52 +0530

    Reading files extracting columns using open() and import csv

---
+++ main.py
@@ -0,0 +1,15 @@
+# with open("weather_data.csv", "r") as files:
+#     data = files.readlines()
+# print(data)
+import csv
+
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    temperature =[]
+    for row in data:
+        if row[1] != "temp":
+            temperature.append(int(row[1]))
+
+print(temperature)
+
+
